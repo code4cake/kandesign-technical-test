ca521112ff38c0e8747e9dbd970e92f336ee9311
DS <ds@DSs-MacBook-Pro.local>
2024-05-30 18:23:23 +0200

    main - add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/kandesign-technical-test/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
